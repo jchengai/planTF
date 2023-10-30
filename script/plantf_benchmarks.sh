86e3b718c0bb1dd750bb22fadcc93e6113669c4c
jchengai <jchengai@connect.ust.hk>
2023-10-30 10:27:45 +0800

    fix benchmark script

--- script/plantf_benchmarks.sh
+++ script/plantf_benchmarks.sh
@@ -8,7 +8,7 @@ CHALLENGES="closed_loop_nonreactive_agents closed_loop_reactive_agents open_loop
 for challenge in $CHALLENGES; do
     python run_simulation.py \
         +simulation=$challenge \
-        planner=$planner \
+        planner=$PLANNER \
         scenario_builder=nuplan_challenge \
         scenario_filter=$SPLIT \
         worker.threads_per_node=20 \
